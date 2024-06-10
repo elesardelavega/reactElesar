808f2be93e484a928bbeaa76c3eb339892ab728c
Elesar <elesar.delavega@flat101.es>
2024-06-10 20:56:38 +0200

    extract to components

--- elesar-tic-tac-toe/src/App.jsx
+++ elesar-tic-tac-toe/src/App.jsx
@@ -1,35 +1,10 @@
 import { useState } from "react"
 import confetti from 'canvas-confetti'
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+import { Square } from "./components/Square.jsx"
+import { TURNS } from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
 
-const Square = ({ children, isSelected, updateBoard, index }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleclick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleclick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
 
 function App() {
   // estado de cuadrados
@@ -41,31 +16,10 @@ function App() {
  // null no hay ganador, false empate
   const [winner, setWinner] = useState(null)
 
-  const checkWinner = (boardToCheck) => {
-    // revisar combinaciones ganadores
-    for (const combo of WINNER_COMBOS){
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ){
-        return boardToCheck[a]
-      }
-    }
-    // no hay ganador
-    return null
-  }
-
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-  }
-
-  const checkEndGame = (newBoard) => {
-    // revisar si hay empate
-    return newBoard.every((square) => square != null)
   }
 
   const updateBoard = (index) => {
@@ -82,7 +36,7 @@ function App() {
     setTurn(newTurn)
 
     // rervisar ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner){
       confetti()
       setWinner(newWinner) // actualiza el estado (asincrono)
@@ -112,27 +66,8 @@ function App() {
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
-        {
-          winner != null && (
-            <section className="winner">
-              <div className="text">
-                <h2>
-                  {
-                    winner === false ? 'Empate' : 'Ganó:'
-                  }
-                </h2>
+      <WinnerModal resetGame={resetGame} winner={winner} />
 
-                <header className="win">
-                  {winner && <Square>{winner}</Square>}
-                </header>
-
-                <footer>
-                  <button onClick={resetGame}>Empezar de nuevo</button>
-                </footer>
-              </div>
-            </section>
-          )
-        }
     </main>
   )
 }
