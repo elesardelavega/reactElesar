c87a0a8729d14fec22d65e4b2a5d361148c2584c
Elesar <elesar.delavega@flat101.es>
2024-06-11 18:19:44 +0200

    add 02 src

--- projects/02-elesar-tic-tac-toe/src/App.jsx
+++ projects/02-elesar-tic-tac-toe/src/App.jsx
@@ -1,34 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import confetti from 'canvas-confetti'
+import { Square } from "./components/Square.jsx"
+import { TURNS } from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from "./logic/index.js"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // estado de cuadrados
+  // useState nunca debe estar dentro de un if (SIEMPRE en el cuerpo del componente)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+
+  // estado de turnos
+  const [turn, setTurn] = useState(() => {
+    const turnFormStorage = window.localStorage.getItem('turn')
+    return turnFormStorage ?? TURNS.X
+  })
+
+ // null no hay ganador, false empate
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    resetGameStorage()
+  }
+
+  const updateBoard = (index) => {
+    // no actualizamos si existe algo
+    if (board[index] || winner) return
+
+    // actualizar tablero
+    const newBoard = [... board]
+    newBoard[index] = turn
+    setBoard(newBoard) // asincrona
+
+    // cambiar turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // guardar aquí partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
+    // rervisar ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner){
+      confetti()
+      setWinner(newWinner) // actualiza el estado (asincrono)
+    } else if (checkEndGame(newBoard)){
+        setWinner(false)
+    }
+  }
+
+  /*useEffect (() => {
+    como mínimo se ejecuta una vez
+    console.log('useEffect')
+  }, [dependencia, por ejemplo: WINNER])*/
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
+
+    </main>
   )
 }
 
---
+++ projects/02-elesar-tic-tac-toe/src/useEffect.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+const Component = () => {
+
+    const [value, setValue] = useState(false)
+
+    useEffect(
+        // function que se ejecuta una vez
+        // código a ejecutar cada vez que se renderice el componente
+        // (codeToExecute)
+        //, 
+        // opcional (listOfDependencies)
+    )
+
+}
