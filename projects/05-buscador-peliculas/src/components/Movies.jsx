1495c1e8a9a3455351468850634a859179b11fcf
Elesar De La Vega <elesarvega@gmail.com>
2025-10-21 13:02:19 +0200

    feat: :tada: add new project buscador peliculas

---
+++ projects/05-buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,23 @@
+export function ListOfMovies({ movies }) {
+  return (
+    <ul className="movies">
+      {movies.map((movie) => (
+        <li key={movie.id} className="movie">
+          <h3>{movie.title}</h3>
+          <p>Año: {movie.year}</p>
+          <img src={movie.image} alt={movie.title} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export function NoMoviesResults() {
+  return <p>No se han encontrado peliculas</p>;
+}
+
+export function Movies({ movies }) {
+  const hasMovies = movies?.length > 0;
+
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />;
+}
